3497019204340111eb9e8792e753687466f8b34f
Anton04 <anton.gustafsson@tii.se>
2014-04-21 21:05:48 +0200

    Update MQTT2StepperMotor.py

--- MQTT2StepperMotor.py
+++ MQTT2StepperMotor.py
@@ -13,42 +13,62 @@ from uuid import getnode as get_mac
 
 class MQTTMotorControl(mosquitto.Mosquitto,MotorControl):
   
-  def __init__(self,Pins = [24,25,8,7],ip = "localhost", port = 1883, clientId = "MQTT2StepperMotor", user = "driver", password = "1234", prefix = "StepperMotor"):
-    mosquitto.Mosquitto.__init__(self,clientId)
+	def __init__(self,Pins = [24,25,8,7],ip = "localhost", port = 1883, clientId = "MQTT2StepperMotor", user = "driver", password = "1234", prefix = "StepperMotor"):
+		mosquitto.Mosquitto.__init__(self,clientId)
+		
+		MotorControl.__init__(self,Pins):
     
-    #Get mac adress. 
-    mac = get_mac()
+    		#Get mac adress. 
+    		mac = get_mac()
     
-    #Make a number based on pins used.
-    pinid = ""
-    for pin in Pins:
-      pinid += "%02i" % pin
+    		#Make a number based on pins used.
+    		pinid = ""
+    		for pin in Pins:
+      			pinid += "%02i" % pin
     
-    self.prefix = prefix + "/" + str(mac) + "/" + pinid
+    		self.prefix = prefix + "/" + str(mac) + "/" + pinid
 		self.ip = ip
-    self.port = port
-    self.clientId = clientId
+    		self.port = port
+    		self.clientId = clientId
 		self.user = user
-    self.password = password
+    		self.password = password
     		
-    if user != None:
-    		self.username_pw_set(user,password)
+    		if user != None:
+    			self.username_pw_set(user,password)
 
 		self.will_set( topic =  "system/" + self.prefix, payload="Offline", qos=1, retain=True)
 		self.will_set( topic =  self.prefix, payload="Offline", qos=1, retain=True)
-    print "Connecting"
-    self.connect(ip,keepalive=10)
-    self.subscribe(self.prefix + "/#", 0)
-    self.on_connect = self.mqtt_on_connect
-    self.on_message = self.mqtt_on_message
-    self.publish(topic = "system/"+ self.prefix, payload="Online", qos=1, retain=True)
-    self.publish(topic = self.prefix, payload="Online", qos=1, retain=True)
+		print "Connecting"
+    		self.connect(ip,keepalive=10)
+    		self.subscribe(self.prefix + "/#", 0)
+    		self.on_connect = self.mqtt_on_connect
+    		self.on_message = self.mqtt_on_message
+    		self.publish(topic = "system/"+ self.prefix, payload="Online", qos=1, retain=True)
+    		self.publish(topic = self.prefix, payload="Online", qos=1, retain=True)
+    
+    
+    		self.loop_start()
+    
+	def mqtt_on_connect(self, selfX,mosq, result):
+    		print "MQTT connected!"
+    		self.subscribe(self.prefix + "/#", 0)
     		
+    	def mqtt_on_message(self, selfX,mosq, msg):
+    		print("RECIEVED MQTT MESSAGE: "+msg.topic + " " + str(msg.payload))
     		
-
-		#thread.start_new_thread(self.ControlLoop,())	
-		self.loop_start()
-
+    		topics = msg.topic.split("/")
+    		
+    		if len(topics[-1]) == 0:
+    			topics = topics[:-1]
+    		
+    		if topics[-1] == "speed"
+    			self.speed = int(msg.payload)
+    		elif topics[-1] == "step"
+    			self.stepN(int(msg.payload))
+    		elif topics[-1] == "moveto"
+    			self.MoveTo(int(msg.payload))
+    		
+    		return
 
 if __name__ == "__main__":
 
